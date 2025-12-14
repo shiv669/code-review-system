d2ad7b4da8eb8592bc38ebe1322ec26dc24e715a
Shivam Gawali <shiv669@users.noreply.github.com>
2025-12-14 10:34:40 +0000

    add session endpoint

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ app.use(express.json());
 
 app.get("/",(req,res)=>{res.json({status: "ok"})});
 
+app.post("/sessions", (req, res)=>{
+  const { author_id } = req.body;
+  try{
+    const result = db.run(
+      `
+      insert into review_session(author_id, status)
+      values(?,"open")
+      `,
+      [author_id]
+    );
+    res.status(201).json(
+      {
+        session_id: result.lastId
+      }
+    );
+  } catch(error) {
+    res.status(500).json(
+      {
+        error: error.message
+      }
+    );
+  }
+});
+
 app.listen(3000, ()=> {
   console.log("server is running");
 });
