465c90c374b70675ff6b69e587869c6713bace80
Shivam Gawali <142748461+shiv669@users.noreply.github.com>
2025-12-19 00:43:51 +0530

    chore: make vite host configuration environment agnostic

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      "b0f3713b-8529-405f-9355-6dcc12b6e7fd-00-263pxhsc8m6nb.pike.replit.dev"
-    ]
+    allowedHosts: true
   }
 });
